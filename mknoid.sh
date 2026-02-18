5857c74343c0fc9bde0a652bc233a408bcf090d8
Naz <ndpm13@ch-naseem.com>
2026-02-18 21:46:04 +0100

    fix: remove redundant mkiso call and add DE validation

--- mknoid.sh
+++ mknoid.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "$#" -eq 0 ]]; then
+if [[ "$#" -lt 2 ]]; then
   echo "Error: No argument provided"
   echo "Usage: $0 <desktop_environment> <path_to_img>"
   exit 1
@@ -12,28 +12,22 @@ repo="https://github.com/noid-linux/xbps-repo/releases/latest/download"
 pkgs="neovim alacritty starship brave ndpm flatpak gparted noid-welcome calamares bazaar"
 xfce_pkgs="$pkgs gruvbox-material-theme gruvbox-bibata-cursor-theme nerd-fonts-cascadiacode"
 
-yes | ./mkiso.sh -a x86_64 -b "$de" \
-	-r "$repo" -- -p "$pkgs" -I noid/"$de" \
-	-C "live.autologin" -T "Noid Linux" \
-	-v "linux-lts" -o "$path"
-
 case $de in
 	"xfce")
 		yes | ./mkiso.sh -a x86_64 -b "xfce" \
-						 -r "$repo" -- -p "$xfce_pkgs" -I noid/xfce \
-						 -C "live.autologin" -T "Noid Linux" \
-						 -v "linux-lts" -o "$path"
-			;;
-	"kde")
-		yes | ./mkiso.sh -a x86_64 -b "kde" \
-						 -r "$repo" -- -p "$pkgs" -I noid/kde \
-						 -C "live.autologin" -T "Noid Linux" \
-						 -v "linux-lts" -o "$path"
-			;;
-	"gnome")
-		yes | ./mkiso.sh -a x86_64 -b "gnome" \
-						 -r "$repo" -- -p "$pkgs" -I noid/gnome \
-						 -C "live.autologin" -T "Noid Linux" \
-						 -v "linux-lts" -o "$path"
-			;;
+			-r "$repo" -- -p "$xfce_pkgs" -I noid/xfce \
+			-C "live.autologin" -T "Noid Linux" \
+			-v "linux-lts" -o "$path"
+		;;
+	"kde"|"gnome")
+		yes | ./mkiso.sh -a x86_64 -b "$de" \
+			-r "$repo" -- -p "$pkgs" -I noid/"$de" \
+			-C "live.autologin" -T "Noid Linux" \
+			-v "linux-lts" -o "$path"
+		;;
+	*)
+		echo "Error: Unknown desktop environment '$de'"
+		echo "Supported: xfce, kde, gnome"
+		exit 1
+		;;
 esac
